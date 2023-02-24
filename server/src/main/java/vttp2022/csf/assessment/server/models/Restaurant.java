718707c241263cca7cf8663654509d08ea96c42a
adilzuhri <adilzuhri@gmail.com>
2023-02-24 12:17:44 +0800

    added on backend

--- server/src/main/java/vttp2022/csf/assessment/server/models/Restaurant.java
+++ server/src/main/java/vttp2022/csf/assessment/server/models/Restaurant.java
@@ -1,5 +1,10 @@
 package vttp2022.csf.assessment.server.models;
 
+import org.bson.Document;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+
 // Do not modify this class
 public class Restaurant {
 	
@@ -51,4 +56,41 @@ public class Restaurant {
 	public String getMapURL() {
 		return this.mapUrl;
 	}
+
+	@Override
+    public String toString() {
+        return "restaurant [rid= "
+                .concat("" + this.restaurantId)
+                .concat(", name=")
+                .concat(this.name)
+                .concat(", cuisine=".replaceAll("/", "_"))
+                .concat("" + this.cuisine)
+                .concat(", address=")
+                .concat("" + this.address)
+                .concat(", coordinates=")
+                .concat("" + this.getCoordinates())
+                .concat(", mapUrl=")
+                .concat(this.mapUrl);
+    }
+
+    public static Restaurant create(Document d) {
+        Restaurant r = new Restaurant();
+        r.setRestaurantId(d.getString("rid"));
+        r.setCuisine(d.getString("name"));
+		r.setAddress(d.getString("address"));
+		//r.setCoordinates(d.latLang("coordinates"));
+		r.setMapURL(d.getString("mapUrl"));
+        return r;
+    }
+
+    public JsonObject toJSON() {
+        return Json.createObjectBuilder()
+                .add("rid", getRestaurantId())
+                .add("name", getName())	
+                .add("cuisine", getCuisine())
+                .add("address", getAddress())
+				.add("mapUrl", getMapURL())
+				// .add("coordinates"), getCoordinates())
+                .build();
+    }
 }
